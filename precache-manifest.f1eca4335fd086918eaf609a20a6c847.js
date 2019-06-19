5a1886734353366c7249c660bda562094215c753
OmarShezan <b4020327@my.shu.ac.uk>
2019-06-19 15:38:31 +0100

    Updates

---
+++ precache-manifest.f1eca4335fd086918eaf609a20a6c847.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d055a291f588f5c42607a431f124466c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5a658572cd768005e3ec",
+    "url": "/portfolio/static/css/2.38750c1c.chunk.css"
+  },
+  {
+    "revision": "56b15c0fac68033c7f1f",
+    "url": "/portfolio/static/css/main.b586ce44.chunk.css"
+  },
+  {
+    "revision": "5a658572cd768005e3ec",
+    "url": "/portfolio/static/js/2.98d60d81.chunk.js"
+  },
+  {
+    "revision": "56b15c0fac68033c7f1f",
+    "url": "/portfolio/static/js/main.e715692d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "05d5b02f47bd873765bb76a6d6449ec8",
+    "url": "/portfolio/static/media/Arrow.05d5b02f.svg"
+  },
+  {
+    "revision": "f33429aab678c5ecf1c8be72c5787c58",
+    "url": "/portfolio/static/media/Dark.f33429aa.jpg"
+  },
+  {
+    "revision": "ea1639a459bfbda779bb578df41e9538",
+    "url": "/portfolio/static/media/coding.ea1639a4.svg"
+  },
+  {
+    "revision": "d21e6802afd4703b7e13c55d38c8fe66",
+    "url": "/portfolio/static/media/education.d21e6802.svg"
+  },
+  {
+    "revision": "895fa1332f4a58b578ae6675eb602911",
+    "url": "/portfolio/static/media/eventTracker.895fa133.png"
+  },
+  {
+    "revision": "128c7ca7fb3a3e698d92d542aaa40a69",
+    "url": "/portfolio/static/media/facebook.128c7ca7.svg"
+  },
+  {
+    "revision": "317608b275375421f70e73be8872952c",
+    "url": "/portfolio/static/media/github.317608b2.svg"
+  },
+  {
+    "revision": "c9cf53251f3d1943f10d8c65b7aa5d3a",
+    "url": "/portfolio/static/media/homebg.c9cf5325.jpg"
+  },
+  {
+    "revision": "754dbb7234dfb05cbc02e71f00cd8937",
+    "url": "/portfolio/static/media/instagram.754dbb72.svg"
+  },
+  {
+    "revision": "f5dc6e91cbb8056b2eaa3e29ab2b789d",
+    "url": "/portfolio/static/media/linkedIn.f5dc6e91.svg"
+  },
+  {
+    "revision": "2fb08a4d66e3925e5b138b21296edee3",
+    "url": "/portfolio/static/media/me.2fb08a4d.jpg"
+  },
+  {
+    "revision": "7a757661a8de1c6f1635b06131d11c86",
+    "url": "/portfolio/static/media/microbit.7a757661.jpg"
+  },
+  {
+    "revision": "de44288834bb3a6ebceb2d288900e815",
+    "url": "/portfolio/static/media/unsplash.de442888.svg"
+  },
+  {
+    "revision": "f2b726f5e2c6e21e14ff38f68bd56441",
+    "url": "/portfolio/static/media/work.f2b726f5.svg"
+  }
+]);
